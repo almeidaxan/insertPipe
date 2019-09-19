f464cee75560d738a91b740b2aa494da80426998
Alexandre Almeida <almeida.xan@gmail.com>
2019-09-19 15:20:08 -0300

    Refactoring before_/after_space functions + fixing missing source error

--- R/utililtes.R
+++ R/utililtes.R
@@ -1,47 +1,52 @@
+after_space <- function() {
+  context_info <- get_context_info()
+  current_row <- context_info$current_row
+  end_column <- context_info$end_column
 
-beforeSpace <- function() {
-
-  current_context <- rstudioapi::getSourceEditorContext()
-  end_row <-
-    as.integer(unlist(current_context$selection)["range.end.row"])
-  end_column <-
-    as.integer(unlist(current_context$selection)["range.end.column"])
-  # browser()
-  curren_row = current_context$contents[end_row]
-  last_character <- ""
-
-  if(end_column > 1)
-  {
-  last_character <-
-    substr(
-      curren_row,
-      end_column-1,
-      end_column-1)
+  if (end_column <= nchar(current_row)) {
+    last_character <- substr(
+      current_row,
+      end_column,
+      end_column
+    )
+  } else {
+    last_character <- ""
   }
 
-  return(ifelse(grepl("^\\s+$", last_character),""," "))
+  ifelse(grepl("^\\s+$", last_character), "", " ")
 }
 
-afterSpace <- function() {
+before_space <- function() {
+  context_info <- get_context_info()
+  current_row <- context_info$current_row
+  end_column <- context_info$end_column
 
-  current_context <- rstudioapi::getSourceEditorContext()
-  end_row <-
-    as.integer(unlist(current_context$selection)["range.end.row"])
-  end_column <-
-    as.integer(unlist(current_context$selection)["range.end.column"])
-
-  curren_row = current_context$contents[end_row]
-  last_character <- ""
-
-  if(end_column <= nchar(curren_row))
-  {
-    last_character <-
-      substr(
-        curren_row,
-        end_column,
-        end_column)
+  if (end_column > 1) {
+    last_character <- substr(
+      current_row,
+      end_column - 1,
+      end_column - 1
+    )
+  } else {
+    last_character <- ""
   }
 
-  return(ifelse(grepl("^\\s+$", last_character),""," "))
+  ifelse(grepl("^\\s+$", last_character), "", " ")
 }
 
+get_context_info <- function() {
+  current_context <- rstudioapi::getActiveDocumentContext()
+
+  end_row <- as.integer(
+    unlist(current_context$selection)["range.end.row"]
+  )
+
+  end_column <- as.integer(
+    unlist(current_context$selection)["range.end.column"]
+  )
+
+  list(
+    current_row = current_context$contents[end_row],
+    end_column = end_column
+  )
+}
